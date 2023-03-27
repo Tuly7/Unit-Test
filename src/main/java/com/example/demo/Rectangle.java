1d26cc42427dca009c4d7b579474a88aef9912e8
Tuly7 <cindetul@students.zhaw.ch>
2023-03-27 13:36:31 +0200

    Code3

--- src/main/java/com/example/demo/Rectangle.java
+++ src/main/java/com/example/demo/Rectangle.java
@@ -6,11 +6,17 @@ public class Rectangle {
     private int height;
     
     public Rectangle(int width, int height) {
+        if (width <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Side lengths must be positive");
+        }
         this.width = width;
         this.height = height;
     }
-
+    
     public Rectangle(int sideLength) {
+        if (sideLength <= 0) {
+            throw new IllegalArgumentException("Side length must be positive");
+        }
         this.width = sideLength;
         this.height = sideLength;
     }
